c06924564da7527507b3438e6bb7006a887aea66
Milton Cruz <mr.cruzm@gmail.com>
2026-05-12 13:52:48 -0700

    Updated stored procs with COALESCE function

--- Stored Procedures/UpdateAddress.sql
+++ Stored Procedures/UpdateAddress.sql
@@ -1,5 +1,6 @@
 USE [Superstore]
 GO
+/****** Object:  StoredProcedure [dbo].[UpdateAddress]    Script Date: 5/12/2026 1:40:53 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -7,21 +8,22 @@ GO
 -- =============================================
 -- Author:		Milton Cruz	
 -- Create date: 4/23/2026
--- Update date: 
+-- Update date: 5/12/2026
 -- Description:	Update an Address
+-- EXEC UpdateAddress @AddressID = 1
 -- EXEC UpdateAddress @AddressID = 1, @AddressLine1 = '123 Main St', @AddressLine2 = 'Apt 4', @City = 'Anytown', @StateID = 1, CountryID = 1, @PostalCode = 12345, @RegionID = 1, @AddressTypeID = 1, @CustomerID = 1, @CustomerID = 1
 -- =============================================
 CREATE PROCEDURE [dbo].[UpdateAddress]
 	@AddressID INT,
-	@AddressLine1 NVARCHAR(25),
-	@AddressLine2 NVARCHAR(25),
-	@City NVARCHAR(50),
-	@StateID INT,
-	@CountryID INT,
-    @PostalCode INT,
-    @RegionID INT,
-    @AddressTypeID INT,
-    @CustomerID  INT
+	@AddressLine1 NVARCHAR(25) = NULL,
+	@AddressLine2 NVARCHAR(25) = NULL,
+	@City NVARCHAR(50) = NULL,
+	@StateID INT = NULL,
+	@CountryID INT = NULL,
+    @PostalCode INT = NULL,
+    @RegionID INT = NULL,
+    @AddressTypeID INT = NULL,
+    @CustomerID  INT = NULL
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
@@ -29,20 +31,20 @@ BEGIN
 	SET NOCOUNT ON;
 
     BEGIN TRY
-    	UPDATE dbo.Address
-		SET AddressLine1 = @AddressLine1, 
-			AddressLine2 = @AddressLine2, 
-			City = @City, 
-			StateID = @StateID, 
-			CountryID = @CountryID, 
-			PostalCode = @PostalCode, 
-			RegionID = @RegionID, 
-			AddressTypeID = @AddressTypeID, 
-			CustomerID = @CustomerID,
+    		UPDATE dbo.Address
+		SET AddressLine1 = COALESCE(@AddressLine1, AddressLine1), 
+			AddressLine2 = COALESCE(@AddressLine2, AddressLine2), 
+			City = COALESCE(@City, City), 
+			StateID = COALESCE(@StateID, StateID), 
+			CountryID = COALESCE(@CountryID, CountryID), 
+			PostalCode = COALESCE(@PostalCode, PostalCode), 
+			RegionID = COALESCE(@RegionID, RegionID), 
+			AddressTypeID = COALESCE(@AddressTypeID, AddressTypeID), 
+			CustomerID = COALESCE(@CustomerID, CustomerID),
 			DateUpdated = GETDATE()
 		WHERE AddressID = @AddressID;
 	END TRY
 	BEGIN CATCH
-    	SELECT ERROR_MESSAGE() AS ErrorMessage;
+   		SELECT ERROR_MESSAGE() AS ErrorMessage;
 	END CATCH;
 END
